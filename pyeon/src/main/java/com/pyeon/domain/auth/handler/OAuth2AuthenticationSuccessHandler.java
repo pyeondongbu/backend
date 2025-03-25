56d595405e1a1ad5190fb7631a76db6a67f39ab9
ChisaeHwang <cwh73090@naver.com>
2025-03-26 07:58:31 +0900

    fix: Oauth 인증 핸들러 코드 구조 문제 해결

--- pyeon/src/main/java/com/pyeon/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
+++ pyeon/src/main/java/com/pyeon/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -2,6 +2,7 @@ package com.pyeon.domain.auth.handler;
 
 import com.pyeon.domain.auth.dto.response.TokenResponse;
 import com.pyeon.domain.auth.service.AuthService;
+import com.pyeon.global.exception.CustomException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +13,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.util.UriComponentsBuilder;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
-import java.net.URI;
 
 @Slf4j
 @Component
@@ -30,33 +28,60 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
             HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication
-    ) throws IOException {
-        log.info("OAuth2 인증 성공: {}", authentication.getName());
-        log.info("인증 객체 클래스: {}", authentication.getClass().getName());
-        log.info("인증 객체 세부 정보: {}", authentication.getDetails());
-        log.info("인증 객체 권한: {}", authentication.getAuthorities());
-        
-        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-        log.info("OAuth2User 클래스: {}", oauth2User.getClass().getName());
-        log.info("OAuth2User 정보: {}", oauth2User.getAttributes());
-        
-        log.info("리다이렉트 URI 설정값: {}", redirectUri);
-        
+    ) {
         try {
-            TokenResponse tokenResponse = authService.createToken(oauth2User);
-            log.info("토큰 생성 완료: {}", tokenResponse.getAccessToken().substring(0, 10) + "...");
-
-            // 토큰을 URL 파라미터로 추가하여 리다이렉트
-            String targetUrl = UriComponentsBuilder.fromUriString(redirectUri)
-                    .queryParam("token", tokenResponse.getAccessToken())
-                    .build().toUriString();
-            
-            log.info("최종 리다이렉트 URL: {}", targetUrl);
-
+            String targetUrl = handleSuccessfulAuthentication(authentication);
             getRedirectStrategy().sendRedirect(request, response, targetUrl);
         } catch (Exception e) {
-            log.error("인증 성공 처리 중 오류 발생", e);
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "인증 처리 중 오류가 발생했습니다.");
+            handleAuthenticationError(request, response, e);
         }
+    }
+
+    private String handleSuccessfulAuthentication(Authentication authentication) {
+        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+        log.info("OAuth2User 정보: {}", oauth2User.getAttributes());
+        
+        TokenResponse tokenResponse = authService.createToken(oauth2User);
+        return buildSuccessUrl(tokenResponse);
+    }
+
+    private String buildSuccessUrl(TokenResponse tokenResponse) {
+        return UriComponentsBuilder.fromUriString(redirectUri)
+                .queryParam("token", tokenResponse.getAccessToken())
+                .build().toUriString();
+    }
+
+    private void handleAuthenticationError(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Exception exception
+    ) {
+        String errorUrl = buildErrorUrl(exception);
+        try {
+            getRedirectStrategy().sendRedirect(request, response, errorUrl);
+        } catch (Exception e) {
+            log.error("리다이렉트 중 오류 발생", e);
+        }
+    }
+
+    private String buildErrorUrl(Exception exception) {
+        if (exception instanceof CustomException customException) {
+            return buildCustomErrorUrl(customException);
+        }
+        return buildGenericErrorUrl();
+    }
+
+    private String buildCustomErrorUrl(CustomException exception) {
+        return UriComponentsBuilder.fromUriString(redirectUri)
+                .queryParam("error", exception.getErrorCode().name())
+                .queryParam("message", exception.getMessage())
+                .build().toUriString();
+    }
+
+    private String buildGenericErrorUrl() {
+        return UriComponentsBuilder.fromUriString(redirectUri)
+                .queryParam("error", "AUTHENTICATION_FAILED")
+                .queryParam("message", "인증 처리 중 오류가 발생했습니다.")
+                .build().toUriString();
     }
 } 
